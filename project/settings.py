43af438c70ededd65a6ff426732e2841282ff40f
kazukun2005 <kazuhito2005@icloud.com>
2025-06-15 22:13:42 +0900

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -65,7 +65,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'myproject.urls'
+ROOT_URLCONF = 'project.urls'
 
 TEMPLATES = [
     {
@@ -83,7 +83,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'myproject.wsgi.application'
+WSGI_APPLICATION = 'project.wsgi.application'
 
 
 # Database
